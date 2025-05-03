b61823a7ebb99c14fedbb46e188082e009a65042
wolfiee42 <saifalislam2022@gmail.com>
2025-05-03 16:21:26 +0600

    learning the alternative n scanf, cin in c++

---
+++ basic_c++/input.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a, b;
+    // scanf("%d %d", &a, &b);
+    cin >> a >> b; // scanf alternative "cin"
+    cout << a << " " << b << endl;
+}
