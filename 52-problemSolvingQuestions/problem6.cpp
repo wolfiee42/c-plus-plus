183a24164dafeb01d6e7a44ce6595f6a0d0f759d
wolfiee42 <saifalislam2022@gmail.com>
2025-05-07 07:56:40 +0600

    solved problem number 6

---
+++ 52-problemSolvingQuestions/problem6.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main ()
+{
+	int T;
+	cin >> T;
+
+	int number;
+	int arr[5];
+	for(int i=0;i<T;i++)
+	{
+		cin >> number;
+		for(int j=4;j >=0;j--)
+		{
+
+			int num = number;
+			int x = num % 10;
+			number = num / 10;
+			arr[j] = x;
+		}
+
+
+		int summaton = arr[4]+arr[0];
+		cout << "Sum" << " = " << summaton<<endl;
+	}
+
+
+  return 0;
+}
