5302dad6b60ed0d52de694507b7ae87e1e3f966e
wolfiee42 <saifalislam2022@gmail.com>
2025-05-13 22:24:58 +0600

    learning element access in c++

---
+++ stringClassInC++/element_access.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main ()
+{
+	string s;
+	cin >> s;
+	cout << s <<endl;
+
+	// 0th index
+	cout << s[0] << endl;
+	cout << s.at(0) << endl;
+	cout << s.front() << endl;
+
+	// last index
+	cout << s[s.size()-1] << endl;
+	cout << s.back() <<endl;
+  return 0;
+}
