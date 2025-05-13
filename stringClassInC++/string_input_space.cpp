478bb21a69095d16adfdff1c403714f547aab846
wolfiee42 <saifalislam2022@gmail.com>
2025-05-13 22:29:59 +0600

    learning how to take input string with space.

---
+++ stringClassInC++/string_input_space.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main ()
+{
+  int a;
+  string s;
+  
+  cin >> a;
+  cin.ignore();
+  getline(cin, s);
+
+  cout << a << endl;
+  cout << s <<endl;
+  return 0;
+}
---
+++ stringClassInC++/stringstream.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main ()
+{
+  string a;
+  getline(cin, a);
+// cout << a;
+
+  stringstream ss;
+  ss << a;
+  string word;
+
+int count =0;
+  while(ss >> word)
+  {
+  	cout << word << endl;
+  	count++;
+  }
+
+cout << count << endl;
+  return 0;
+}
